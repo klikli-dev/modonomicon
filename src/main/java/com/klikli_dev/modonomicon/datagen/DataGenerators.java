94de4c6be93055e7d4470e68e3ae4fecfa269fe8
Kli Kli <klikli@outlook.at>
2022-11-30 17:15:12 +0100

    Add comment

--- src/main/java/com/klikli_dev/modonomicon/datagen/DataGenerators.java
+++ src/main/java/com/klikli_dev/modonomicon/datagen/DataGenerators.java
@@ -18,11 +18,12 @@ public class DataGenerators {
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
 
         var lang =  new LangGenerator.English(generator);
-        generator.addProvider(event.includeClient(), new ItemModelsGenerator(generator, existingFileHelper));
 
         generator.addProvider(event.includeServer(), new AdvancementsGenerator(generator));
         generator.addProvider(event.includeServer(), new DemoBookProvider(generator, Modonomicon.MODID, lang));
-        generator.addProvider(event.includeClient(), lang);
 
+        //Important: Lang provider needs to be added after the book provider to process the texts added by the book provider
+        generator.addProvider(event.includeClient(), lang);
+        generator.addProvider(event.includeClient(), new ItemModelsGenerator(generator, existingFileHelper));
     }
 }
