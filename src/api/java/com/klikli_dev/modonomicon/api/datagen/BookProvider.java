b43b4f83f02a37aa8df9fae95a17ade3a0cd2b74
Kli Kli <klikli@outlook.at>
2022-12-09 15:56:37 +0100

    feat: update book provider

--- src/api/java/com/klikli_dev/modonomicon/api/datagen/BookProvider.java
+++ src/api/java/com/klikli_dev/modonomicon/api/datagen/BookProvider.java
@@ -18,9 +18,10 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.data.LanguageProvider;
 import org.slf4j.Logger;
 
-import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
@@ -34,7 +35,6 @@ public abstract class BookProvider implements DataProvider {
     protected final String modid;
 
     /**
-     *
      * @param lang The LanguageProvider to fill with this book provider. IMPORTANT: the Languag Provider needs to be added to the DataGenerator AFTER the BookProvider.
      */
     public BookProvider(DataGenerator generator, String modid, LanguageProvider lang) {
@@ -79,36 +79,29 @@ public abstract class BookProvider implements DataProvider {
     @Override
     public CompletableFuture<?> run(CachedOutput cache) {
 
-        Path folder = this.generator.getOutputFolder();
+        List<CompletableFuture<?>> futures = new ArrayList<>();
+
+
+        Path folder = this.generator.getPackOutput().getOutputFolder();
 
         this.generate();
 
         for (var bookModel : this.bookModels.values()) {
-            Path bookPath = getPath(folder, bookModel);
-            try {
-                DataProvider.saveStable(cache, bookModel.toJson(), bookPath);
-            } catch (IOException exception) {
-                LOGGER.error("Couldn't save book {}", bookPath, exception);
-            }
+            Path bookPath = this.getPath(folder, bookModel);
+            futures.add(DataProvider.saveStable(cache, bookModel.toJson(), bookPath));
 
             for (var bookCategoryModel : bookModel.getCategories()) {
-                Path bookCategoryPath = getPath(folder, bookCategoryModel);
-                try {
-                    DataProvider.saveStable(cache, bookCategoryModel.toJson(), bookCategoryPath);
-                } catch (IOException exception) {
-                    LOGGER.error("Couldn't save book category {}", bookCategoryPath, exception);
-                }
+                Path bookCategoryPath = this.getPath(folder, bookCategoryModel);
+                futures.add(DataProvider.saveStable(cache, bookCategoryModel.toJson(), bookCategoryPath));
 
                 for (var bookEntryModel : bookCategoryModel.getEntries()) {
-                    Path bookEntryPath = getPath(folder, bookEntryModel);
-                    try {
-                        DataProvider.saveStable(cache, bookEntryModel.toJson(), bookEntryPath);
-                    } catch (IOException exception) {
-                        LOGGER.error("Couldn't save book entry {}", bookEntryPath, exception);
-                    }
+                    Path bookEntryPath = this.getPath(folder, bookEntryModel);
+                    futures.add(DataProvider.saveStable(cache, bookEntryModel.toJson(), bookEntryPath));
                 }
             }
         }
+
+        return CompletableFuture.allOf(futures.toArray(CompletableFuture[]::new));
     }
 
     @Override
