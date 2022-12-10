3e0cad1f81e61dfcd28956786cdda3e74a427ce5
Kli Kli <klikli@outlook.at>
2022-12-10 15:37:43 +0100

    feat: update baked book model

--- src/main/java/com/klikli_dev/modonomicon/client/BookBakedModel.java
+++ src/main/java/com/klikli_dev/modonomicon/client/BookBakedModel.java
@@ -8,14 +8,17 @@ package com.klikli_dev.modonomicon.client;
 
 import com.klikli_dev.modonomicon.book.Book;
 import com.klikli_dev.modonomicon.item.ModonomiconItem;
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.BlockModel;
 import net.minecraft.client.renderer.block.model.ItemOverrides;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.client.resources.model.ModelBaker;
 import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.core.Direction;
@@ -24,6 +27,7 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.client.model.data.ModelData;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -39,7 +43,8 @@ public class BookBakedModel implements BakedModel {
         this.original = original;
         BlockModel missing = (BlockModel) loader.getModel(ModelBakery.MISSING_MODEL_LOCATION);
 
-        this.overrides = new ItemOverrides(loader, missing, id -> missing, Collections.emptyList()) {
+        //TODO: test
+        this.overrides = new ItemOverrides(null, missing, Collections.emptyList()) {
             @Override
             public BakedModel resolve(@NotNull BakedModel original, @NotNull ItemStack stack,
                                       @Nullable ClientLevel world, @Nullable LivingEntity entity, int seed) {
@@ -53,9 +58,15 @@ public class BookBakedModel implements BakedModel {
         };
     }
 
+
     @Override
     public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, RandomSource random) {
         return this.original.getQuads(state, side, random);
+    }
+
+    @Override
+    public @NotNull List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, @NotNull RandomSource rand, @NotNull ModelData data, @Nullable RenderType renderType) {
+        return this.original.getQuads(state, side, rand, data, renderType);
     }
 
     @Override
@@ -87,6 +98,11 @@ public class BookBakedModel implements BakedModel {
     @Override
     public ItemTransforms getTransforms() {
         return this.original.getTransforms();
+    }
+
+    @Override
+    public BakedModel applyTransform(ItemTransforms.TransformType transformType, PoseStack poseStack, boolean applyLeftHandTransform) {
+        return this.original.applyTransform(transformType, poseStack, applyLeftHandTransform);
     }
 
     @Override
