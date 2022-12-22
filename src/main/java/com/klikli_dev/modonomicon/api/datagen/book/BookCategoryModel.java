6084eee3a985cd6c7e5c5989594040bb32cb78f2
Kli Kli <klikli@outlook.at>
2022-12-22 09:34:00 +0100

    feat: add shortcut for cat/entry icons from item and add docs

--- src/main/java/com/klikli_dev/modonomicon/api/datagen/book/BookCategoryModel.java
+++ src/main/java/com/klikli_dev/modonomicon/api/datagen/book/BookCategoryModel.java
@@ -10,6 +10,8 @@ import com.google.gson.JsonObject;
 import com.klikli_dev.modonomicon.api.ModonomiconConstants.Data.Category;
 import com.klikli_dev.modonomicon.api.datagen.book.condition.BookConditionModel;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -103,65 +105,124 @@ public class BookCategoryModel {
         private Builder() {
         }
 
-        public static Builder aBookCategoryModel() {
-            return new Builder();
-        }
 
+        /**
+         * Set the book this category belongs to.
+         */
         public Builder withBook(BookModel book) {
             this.book = book;
             return this;
         }
 
+        /**
+         * Set the id of this category.
+         * The ID must be unique within the book.
+         *
+         * @param id the cateogry ID, e.g. "modonomicon:features"
+         */
         public Builder withId(ResourceLocation id) {
             this.id = id;
             return this;
         }
 
+        /**
+         * Set the name of this category.
+         *
+         * @param name Should be a translation key.
+         */
         public Builder withName(String name) {
             this.name = name;
             return this;
         }
 
+        /**
+         * Sets the category's icon.
+         *
+         * @param icon Either an item ResourceLocation (e.g.: "minecraft:stone") or a texture ResourceLocation (e.g. "minecraft:textures/block/stone.png" - note the ".png" at the end)
+         */
         public Builder withIcon(String icon) {
             this.icon = icon;
             return this;
         }
 
+        /**
+         * Sets the category's icon to the texture of the given item
+         */
+        public Builder withIcon(ItemLike item) {
+            this.icon = ForgeRegistries.ITEMS.getKey(item.asItem()).toString();
+            return this;
+        }
+
+
+        /**
+         * Sets the category's sort number.
+         * Categories with a lower sort number will be displayed first.
+         */
         public Builder withSortNumber(int sortNumber) {
             this.sortNumber = sortNumber;
             return this;
         }
 
+        /**
+         * Sets the category's background texture.
+         * The texture must be a 256x256 png file.
+         * Default value is {@link Category#DEFAULT_BACKGROUND}
+         */
         public Builder withBackground(ResourceLocation background) {
             this.background = background;
             return this;
         }
 
+        /**
+         * Sets the category's entry textures.
+         * The texture must be a 256x256 png file.
+         * This texture is used to display the entry background icons as well as the arrows connecting entries.
+         * Default value is {@link Category#DEFAULT_ENTRY_TEXTURES}
+         */
         public Builder withEntryTextures(ResourceLocation entryTextures) {
             this.entryTextures = entryTextures;
             return this;
         }
 
+        /**
+         * Replaces the current list of entries with the given list.
+         */
         public Builder withEntries(List<BookEntryModel> entries) {
             this.entries.addAll(entries);
             return this;
         }
 
+        /**
+         * Adds the given entries to the list of entries.
+         */
         public Builder withEntries(BookEntryModel... entries) {
             this.entries.addAll(List.of(entries));
             return this;
         }
 
+        /**
+         * Adds the given entry to the list of entries.
+         */
         public Builder withEntry(BookEntryModel entry) {
             this.entries.add(entry);
             return this;
         }
 
+        /**
+         * Sets the condition that needs to be met for this category to be shown.
+         * If no condition is set, the category will be unlocked by default.
+         * Use {@link com.klikli_dev.modonomicon.api.datagen.book.condition.BookAndConditionModel} or {@link com.klikli_dev.modonomicon.api.datagen.book.condition.BookOrConditionModel} to combine multiple conditions.
+         */
         public Builder withCondition(BookConditionModel condition) {
             this.condition = condition;
             return this;
         }
 
+        /**
+         * Sets whether the category button should be shown in the book.
+         * Default value is true.
+         * If false, the category will not have a navigation button, but can still be accessed by clicking on an entry or link that links to it.
+         */
         public Builder withShowCategoryButton(boolean showCategoryButton) {
             this.showCategoryButton = showCategoryButton;
             return this;
