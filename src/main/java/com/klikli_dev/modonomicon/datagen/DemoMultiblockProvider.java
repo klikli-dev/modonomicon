76389de367b41e67beaefb51de57f60b4b85cfb1
Kli Kli <klikli@outlook.at>
2023-04-30 16:08:41 +0200

    chore: add missing licenses and statements for REUSE compliance

--- src/main/java/com/klikli_dev/modonomicon/datagen/DemoMultiblockProvider.java
+++ src/main/java/com/klikli_dev/modonomicon/datagen/DemoMultiblockProvider.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 klikli-dev
+//
+// SPDX-License-Identifier: MIT
+
 package com.klikli_dev.modonomicon.datagen;
 
 import com.klikli_dev.modonomicon.api.datagen.MultiblockProvider;
