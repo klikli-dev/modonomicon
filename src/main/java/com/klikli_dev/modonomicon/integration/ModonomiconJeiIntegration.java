87e000f4ab67240b142e9cacd065b3c2de1a9ce1
Kli Kli <klikli@outlook.at>
2023-06-08 12:34:27 +0200

    chore: update to 1.20 mdk

--- src/main/java/com/klikli_dev/modonomicon/integration/ModonomiconJeiIntegration.java
+++ src/main/java/com/klikli_dev/modonomicon/integration/ModonomiconJeiIntegration.java
@@ -7,22 +7,9 @@
 package com.klikli_dev.modonomicon.integration;
 
 import com.klikli_dev.modonomicon.Modonomicon;
-import com.klikli_dev.modonomicon.api.ModonomiconAPI;
-import com.klikli_dev.modonomicon.api.ModonomiconConstants;
-import com.klikli_dev.modonomicon.registry.ItemRegistry;
-import mezz.jei.api.IModPlugin;
-import mezz.jei.api.JeiPlugin;
-import mezz.jei.api.constants.VanillaTypes;
-import mezz.jei.api.recipe.RecipeIngredientRole;
-import mezz.jei.api.registration.ISubtypeRegistration;
-import mezz.jei.api.runtime.IJeiRuntime;
-import mezz.jei.api.runtime.IRecipesGui;
-import net.minecraft.client.Minecraft;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.registries.ForgeRegistries;
-import org.jetbrains.annotations.NotNull;
 
 public class ModonomiconJeiIntegration {
 
@@ -57,46 +44,51 @@ public class ModonomiconJeiIntegration {
 
     public static class ModonomiconJeiHelper {
         public static void showRecipe(ItemStack stack) {
-            var focus = ModonomiconJeiPlugin.jeiRuntime.getJeiHelpers().getFocusFactory().createFocus(RecipeIngredientRole.OUTPUT, VanillaTypes.ITEM_STACK, stack);
-            ModonomiconJeiPlugin.jeiRuntime.getRecipesGui().show(focus);
+            //TODO: enable when jei is updated
+//            var focus = ModonomiconJeiPlugin.jeiRuntime.getJeiHelpers().getFocusFactory().createFocus(RecipeIngredientRole.OUTPUT, VanillaTypes.ITEM_STACK, stack);
+//            ModonomiconJeiPlugin.jeiRuntime.getRecipesGui().show(focus);
         }
 
         public static void showUses(ItemStack stack) {
-            var focus = ModonomiconJeiPlugin.jeiRuntime.getJeiHelpers().getFocusFactory().createFocus(RecipeIngredientRole.INPUT, VanillaTypes.ITEM_STACK, stack);
-            ModonomiconJeiPlugin.jeiRuntime.getRecipesGui().show(focus);
+            //TODO: enable when jei is updated
+//            var focus = ModonomiconJeiPlugin.jeiRuntime.getJeiHelpers().getFocusFactory().createFocus(RecipeIngredientRole.INPUT, VanillaTypes.ITEM_STACK, stack);
+//            ModonomiconJeiPlugin.jeiRuntime.getRecipesGui().show(focus);
         }
 
         public static boolean isJEIRecipesGuiOpen() {
-            return Minecraft.getInstance().screen instanceof IRecipesGui;
+            //TODO: enable when jei is updated
+            //return Minecraft.getInstance().screen instanceof IRecipesGui;
+            return false;
         }
     }
 
-    @JeiPlugin
-    public static class ModonomiconJeiPlugin implements IModPlugin {
-        private static final ResourceLocation UID = new ResourceLocation(ModonomiconAPI.ID, ModonomiconAPI.ID);
-
-        private static IJeiRuntime jeiRuntime;
-
-        @NotNull
-        @Override
-        public ResourceLocation getPluginUid() {
-            return UID;
-        }
-
-        @Override
-        public void onRuntimeAvailable(@NotNull IJeiRuntime jeiRuntime) {
-            ModonomiconJeiPlugin.jeiRuntime = jeiRuntime;
-        }
-
-        @Override
-        public void registerItemSubtypes(@NotNull ISubtypeRegistration registration) {
-
-            registration.registerSubtypeInterpreter(VanillaTypes.ITEM_STACK, ItemRegistry.MODONOMICON.get(), (stack, context) -> {
-                if (!stack.hasTag() || !stack.getTag().contains(ModonomiconConstants.Nbt.ITEM_BOOK_ID_TAG)) {
-                    return "";
-                }
-                return stack.getTag().getString(ModonomiconConstants.Nbt.ITEM_BOOK_ID_TAG);
-            });
-        }
-    }
+    //TODO: enable when jei is updated
+//    @JeiPlugin
+//    public static class ModonomiconJeiPlugin implements IModPlugin {
+//        private static final ResourceLocation UID = new ResourceLocation(ModonomiconAPI.ID, ModonomiconAPI.ID);
+//
+//        private static IJeiRuntime jeiRuntime;
+//
+//        @NotNull
+//        @Override
+//        public ResourceLocation getPluginUid() {
+//            return UID;
+//        }
+//
+//        @Override
+//        public void onRuntimeAvailable(@NotNull IJeiRuntime jeiRuntime) {
+//            ModonomiconJeiPlugin.jeiRuntime = jeiRuntime;
+//        }
+//
+//        @Override
+//        public void registerItemSubtypes(@NotNull ISubtypeRegistration registration) {
+//
+//            registration.registerSubtypeInterpreter(VanillaTypes.ITEM_STACK, ItemRegistry.MODONOMICON.get(), (stack, context) -> {
+//                if (!stack.hasTag() || !stack.getTag().contains(ModonomiconConstants.Nbt.ITEM_BOOK_ID_TAG)) {
+//                    return "";
+//                }
+//                return stack.getTag().getString(ModonomiconConstants.Nbt.ITEM_BOOK_ID_TAG);
+//            });
+//        }
+//    }
 }
