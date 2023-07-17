abed1f3561e4cb6be115046e99294631118b11cf
Kli Kli <klikli@outlook.at>
2023-07-17 13:50:27 +0200

    feat: add fabric registration factory

--- fabric/src/main/java/com/klikli_dev/modonomicon/ModonomiconFabric.java
+++ fabric/src/main/java/com/klikli_dev/modonomicon/ModonomiconFabric.java
@@ -35,5 +35,6 @@ public class ModonomiconFabric implements ModInitializer {
         //TODO: Fabric: FluidRenderHelper/Tooltip
         //TODO: Fabric: Patchouli
         //TODO: Fabric: reload listeners
+        //TODO: Fabric: advancement
     }
 }
