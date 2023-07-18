8ae8f2e71561a24ec669590f1689159ee2093a9d
Kli Kli <klikli@outlook.at>
2023-07-18 18:57:49 +0200

    fix: mixins

--- fabric/src/main/java/com/klikli_dev/modonomicon/mixin/MixinClientPacketListener.java
+++ fabric/src/main/java/com/klikli_dev/modonomicon/mixin/MixinClientPacketListener.java
@@ -3,16 +3,15 @@ package com.klikli_dev.modonomicon.mixin;
 import com.klikli_dev.modonomicon.data.BookDataManager;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.network.protocol.game.ClientboundUpdateRecipesPacket;
-import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPacketListener.class)
 public abstract class MixinClientPacketListener {
     @Inject(at = @At("TAIL"), method = "handleUpdateRecipes(Lnet/minecraft/network/protocol/game/ClientboundUpdateRecipesPacket;)V")
-    private void handleUpdateRecipes(ClientboundUpdateRecipesPacket pPacket) {
+    private void handleUpdateRecipes(ClientboundUpdateRecipesPacket pPacket, CallbackInfo info) {
         BookDataManager.get().onRecipesUpdated();
     }
 }
--- fabric/src/main/java/com/klikli_dev/modonomicon/mixin/MixinModelManager.java
+++ fabric/src/main/java/com/klikli_dev/modonomicon/mixin/MixinModelManager.java
@@ -1,29 +1,22 @@
 package com.klikli_dev.modonomicon.mixin;
 
 import com.klikli_dev.modonomicon.client.BookModel;
-import com.klikli_dev.modonomicon.client.render.MultiblockPreviewRenderer;
-import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.Camera;
-import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.resources.model.AtlasSet;
 import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.client.resources.model.ModelManager;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.profiling.ProfilerFiller;
-import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
 
 @Mixin(ModelManager.class)
 public abstract class MixinModelManager {
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/model/ModelBakery;getBakedTopLevelModels()Ljava/util/Map;"), method = "loadModels")
-    public void replaceBookModel(ProfilerFiller profilerFiller, Map<ResourceLocation, AtlasSet.StitchResult> map, ModelBakery modelBakery, CallbackInfo info) {
+    public void replaceBookModel(ProfilerFiller profilerFiller, Map<ResourceLocation, AtlasSet.StitchResult> map, ModelBakery modelBakery, CallbackInfoReturnable<?> info) {
         BookModel.replace(modelBakery.getBakedTopLevelModels(), modelBakery);
     }
 }
