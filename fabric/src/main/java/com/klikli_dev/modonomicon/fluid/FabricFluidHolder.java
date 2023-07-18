aba5bccda67f2089218d702dcd1c50ba082564fd
Kli Kli <klikli@outlook.at>
2023-07-18 18:30:11 +0200

    feat: add fabric fluid rendering

---
+++ fabric/src/main/java/com/klikli_dev/modonomicon/fluid/FabricFluidHolder.java
@@ -0,0 +1,72 @@
+package com.klikli_dev.modonomicon.fluid;
+
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.material.Fluid;
+
+public class FabricFluidHolder implements FluidHolder {
+
+    private FluidVariant fluidVariant;
+    private int amount;
+
+    public FabricFluidHolder(FluidHolder fluid) {
+        this(fluid.getFluid(), fluid.getAmount(), fluid.getTag());
+    }
+
+    public FabricFluidHolder(Fluid fluid, int amount, CompoundTag tag) {
+        this(FluidVariant.of(fluid, tag), amount);
+    }
+
+    public FabricFluidHolder(FluidVariant fluidVariant, int amount) {
+        this.fluidVariant = fluidVariant;
+        this.amount = amount;
+    }
+
+    public static FabricFluidHolder empty() {
+        return new FabricFluidHolder(FluidVariant.blank(), 0);
+    }
+
+    public FluidVariant toVariant() {
+        return FluidVariant.of(this.getFluid(), this.getTag());
+    }
+
+    @Override
+    public Fluid getFluid() {
+        return this.fluidVariant.getFluid();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.amount <= 0;
+    }
+
+    @Override
+    public int getAmount() {
+        return this.amount;
+    }
+
+    @Override
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean hasTag() {
+        return this.fluidVariant.hasNbt();
+    }
+
+    @Override
+    public CompoundTag getTag() {
+        return this.fluidVariant.getNbt();
+    }
+
+    @Override
+    public void setTag(CompoundTag tag) {
+        this.fluidVariant = FluidVariant.of(this.fluidVariant.getFluid(), tag);
+    }
+
+    @Override
+    public FluidHolder copy() {
+        return new FabricFluidHolder(this.getFluid(), this.getAmount(), this.getTag());
+    }
+}
