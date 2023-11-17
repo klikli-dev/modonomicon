8a7a15035c39ea90c10d7bc0c198c6ece40a7008
Kli Kli <klikli@outlook.at>
2023-11-17 08:36:09 +0100

    fix: networking

--- neo/src/main/java/com/klikli_dev/modonomicon/network/MessageHandler.java
+++ neo/src/main/java/com/klikli_dev/modonomicon/network/MessageHandler.java
@@ -15,23 +15,23 @@ import java.util.function.Supplier;
 
 public class MessageHandler {
 
-    public static <T extends Message> void handle(T message, Supplier<NetworkEvent.Context> ctx) {
-        if (ctx.get().getDirection().getReceptionSide() == LogicalSide.SERVER) {
-            ctx.get().enqueueWork(() -> {
+    public static <T extends Message> void handle(T message, NetworkEvent.Context ctx) {
+        if (ctx.getDirection().getReceptionSide() == LogicalSide.SERVER) {
+            ctx.enqueueWork(() -> {
                 handleServer(message, ctx);
             });
         } else {
-            ctx.get().enqueueWork(() -> {
+            ctx.enqueueWork(() -> {
                 //separate class to avoid loading client code on server.
                 //Using OnlyIn on a method in this class would work too, but is discouraged
                 ClientMessageHandler.handleClient(message, ctx);
             });
         }
-        ctx.get().setPacketHandled(true);
+        ctx.setPacketHandled(true);
     }
 
-    public static <T extends Message> void handleServer(T message, Supplier<NetworkEvent.Context> ctx) {
-        MinecraftServer server = ctx.get().getSender().level().getServer();
-        message.onServerReceived(server, ctx.get().getSender());
+    public static <T extends Message> void handleServer(T message, NetworkEvent.Context ctx) {
+        MinecraftServer server = ctx.getSender().level().getServer();
+        message.onServerReceived(server, ctx.getSender());
     }
 }
