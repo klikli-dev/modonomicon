5ca073ef4145ca39edaf965591e2ba046fd2f6a9
Kli Kli <klikli@outlook.at>
2024-01-08 09:20:13 +0100

    feat: add entry provider
    allows to do one datagen file per entry instead of per category

---
+++ common/src/main/java/com/klikli_dev/modonomicon/api/datagen/EntryBackground.java
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2023 klikli-dev
+//
+// SPDX-License-Identifier: MIT
+
+package com.klikli_dev.modonomicon.api.datagen;
+
+import com.mojang.datafixers.util.Pair;
+
+public class EntryBackground {
+    public static final Pair<Integer, Integer> DEFAULT = Pair.of(0, 0);
+    public static final Pair<Integer, Integer> CATEGORY_START = Pair.of(0, 1);
+    public static final Pair<Integer, Integer> LINK_TO_CATEGORY = Pair.of(1, 1);
+}
