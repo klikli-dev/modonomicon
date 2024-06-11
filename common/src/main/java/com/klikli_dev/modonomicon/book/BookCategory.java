729d578613ce524cbb7408abd68d3daa79001514
Kli Kli <klikli@outlook.at>
2024-06-11 07:25:55 +0200

    fix: default zoom multiplier wrong by a factor 512
    Closes #201

--- common/src/main/java/com/klikli_dev/modonomicon/book/BookCategory.java
+++ common/src/main/java/com/klikli_dev/modonomicon/book/BookCategory.java
@@ -68,7 +68,7 @@ public class BookCategory {
         var background = new ResourceLocation(GsonHelper.getAsString(json, "background", Category.DEFAULT_BACKGROUND));
         var backgroundWidth = GsonHelper.getAsInt(json, "background_width", Category.DEFAULT_BACKGROUND_WIDTH);
         var backgroundHeight = GsonHelper.getAsInt(json, "background_height", Category.DEFAULT_BACKGROUND_HEIGHT);
-        var backgroundTextureZoomMultiplier = GsonHelper.getAsFloat(json, "background_texture_zoom_multiplier", Category.DEFAULT_BACKGROUND_HEIGHT);
+        var backgroundTextureZoomMultiplier = GsonHelper.getAsFloat(json, "background_texture_zoom_multiplier", Category.DEFAULT_BACKGROUND_TEXTURE_ZOOM_MULTIPLIER);
         var entryTextures = new ResourceLocation(GsonHelper.getAsString(json, "entry_textures", Category.DEFAULT_ENTRY_TEXTURES));
         var showCategoryButton = GsonHelper.getAsBoolean(json, "show_category_button", true);
 
