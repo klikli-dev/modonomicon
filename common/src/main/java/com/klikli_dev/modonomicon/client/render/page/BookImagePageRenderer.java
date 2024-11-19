55b80c1af3396c6cf417565a10ea355800b62763
Kli Kli <klikli@outlook.at>
2024-11-19 17:42:11 +0100

    chore: add comments

--- common/src/main/java/com/klikli_dev/modonomicon/client/render/page/BookImagePageRenderer.java
+++ common/src/main/java/com/klikli_dev/modonomicon/client/render/page/BookImagePageRenderer.java
@@ -63,7 +63,10 @@ public class BookImagePageRenderer extends BookPageRenderer<BookImagePage> imple
         if (this.page.useLegacyRendering())
             guiGraphics.blit(RenderType::guiTextured, this.page.getImages()[this.index], x * 2 + 6, y * 2 + 6, 0, 0, 200, 200, 256, 256);
         else
-            guiGraphics.blit(RenderType::guiTextured, this.page.getImages()[this.index], x * 2 + 6, y * 2 + 6, 0, 0, 200, 200, 200, 200, 200, 200); //TODO: look into this once parchment is up to date. Not sure why we have to add the height + width 3 times instead of 2.
+            //TODO: look into this once parchment is up to date. Not sure why we have to add the height + width 3 times instead of 2.
+            //from experiments in bookicon it seems that the first set of parameters after the "0, 0" is the render size, the second and third are then the size in the texture and the texture size.
+            guiGraphics.blit(RenderType::guiTextured, this.page.getImages()[this.index], x * 2 + 6, y * 2 + 6, 0, 0, 200, 200, 200, 200, 200, 200);
+
         guiGraphics.pose().scale(2F, 2F, 1);
         guiGraphics.pose().popPose();
 
