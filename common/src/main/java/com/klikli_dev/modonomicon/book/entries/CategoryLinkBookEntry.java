49d6ad7aa8de3440672ce5e66c405cc6e1e6719f
Kli Kli <klikli@outlook.at>
2025-02-11 14:40:54 +0100

    fix: NPE caused by category link book entries

--- common/src/main/java/com/klikli_dev/modonomicon/book/entries/CategoryLinkBookEntry.java
+++ common/src/main/java/com/klikli_dev/modonomicon/book/entries/CategoryLinkBookEntry.java
@@ -58,14 +58,14 @@ public class CategoryLinkBookEntry extends BookEntry {
         buffer.writeResourceLocation(this.id);
         this.data.toNetwork(buffer);
         buffer.writeNullable(this.commandToRunOnFirstReadId, FriendlyByteBuf::writeResourceLocation);
-        buffer.writeResourceLocation(this.categoryToOpenId);
+        buffer.writeNullable(this.categoryToOpenId, FriendlyByteBuf::writeResourceLocation); //can be set to null in #build, if the category was not found
     }
     
     public static CategoryLinkBookEntry fromNetwork(FriendlyByteBuf buffer) {
         var id = buffer.readResourceLocation();
         BookEntryData data = BookEntryData.fromNetwork(buffer);
         ResourceLocation commandToRunOnFirstReadId = buffer.readNullable(FriendlyByteBuf::readResourceLocation);
-        ResourceLocation categoryToOpen = buffer.readResourceLocation();
+        ResourceLocation categoryToOpen = buffer.readNullable(FriendlyByteBuf::readResourceLocation); //can be set to null in #build, if the category was not found
 
         return new CategoryLinkBookEntry(id, data, commandToRunOnFirstReadId, categoryToOpen);
     }
