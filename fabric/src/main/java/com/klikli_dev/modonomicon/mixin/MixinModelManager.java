b317c6513e2e40c2281401fef417556161c789f0
Kli Kli <klikli@outlook.at>
2025-03-25 17:27:03 +0100

    fix: mixin model manager

--- fabric/src/main/java/com/klikli_dev/modonomicon/mixin/MixinModelManager.java
+++ fabric/src/main/java/com/klikli_dev/modonomicon/mixin/MixinModelManager.java
@@ -1,9 +1,11 @@
 package com.klikli_dev.modonomicon.mixin;
 
+import com.google.common.collect.Multimap;
 import com.klikli_dev.modonomicon.client.BookModel;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.renderer.SpecialBlockModelRenderer;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.AtlasSet;
 import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.client.resources.model.ModelManager;
@@ -14,6 +16,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.Map;
@@ -26,27 +29,7 @@ public class MixinModelManager {
                     target = "Lnet/minecraft/client/resources/model/ModelManager;createBlockStateToModelDispatch(Ljava/util/Map;Lnet/minecraft/client/resources/model/BakedModel;)Ljava/util/Map;",
                     shift = At.Shift.BEFORE),
             locals = LocalCapture.CAPTURE_FAILEXCEPTION)
-    private void modifyItemModelsAfterBake(ProfilerFiller profiler,
-                                                       Map<ResourceLocation, AtlasSet.StitchResult> atlasPreperations,
-                                                       ModelBakery modelBakery,
-                                                       Object2IntMap<BlockState> modelGroups,
-                                                       EntityModelSet entityModelSet,
-                                                       SpecialBlockModelRenderer specialBlockModelRenderer,
-                                                       CallbackInfo ci,
-                                                       ModelBakery.BakingResult bakingResult) {
-
+    private static void modifyItemModelsAfterBake(ProfilerFiller profiler, Map<ResourceLocation, AtlasSet.StitchResult> atlasPreperations, ModelBakery modelBakery, Object2IntMap<BlockState> modelGroups, EntityModelSet entityModelSet, SpecialBlockModelRenderer specialBlockModelRenderer, CallbackInfoReturnable<ModelManager.ReloadState> cir, Multimap multimap, Multimap multimap2, TextureAtlasSprite textureAtlasSprite, ModelBakery.BakingResult bakingResult) {
         BookModel.replace(bakingResult.itemStackModels());
-        // Your custom code here
     }
-
-//    //Lnet/minecraft/client/resources/model/ModelManager;loadModels(Lnet/minecraft/util/profiling/ProfilerFiller;Ljava/util/Map;Lnet/minecraft/client/resources/model/ModelBakery;Lit/unimi/dsi/fastutil/objects/Object2IntMap;Lnet/minecraft/client/model/geom/EntityModelSet;Lnet/minecraft/client/renderer/SpecialBlockModelRenderer;)Lnet/minecraft/client/resources/model/ModelManager$ReloadState;
-//    @Inject(method = "loadModels",
-//            at = @At(value = "INVOKE",
-//                    target = "Lcom/google/common/collect/Multimap;asMap()Ljava/util/Map;",
-//                    ordinal = 1,
-//                    shift = At.Shift.AFTER),
-//            locals = LocalCapture.CAPTURE_FAILEXCEPTION)
-//    private void modifyItemModelsAfterBake(ProfilerFiller profiler, Map<ResourceLocation, AtlasSet.StitchResult> atlasPreperations, ModelBakery modelBakery, Object2IntMap<BlockState> modelGroups, EntityModelSet entityModelSet, SpecialBlockModelRenderer specialBlockModelRenderer, CallbackInfo ci, ModelBakery.BakingResult bakingResult) {
-//        BookModel.replace(bakingResult.itemStackModels());
-//    }
 }
