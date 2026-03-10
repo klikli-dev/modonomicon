b2fc3cfc8146e42410dbfa7c15572a434f67871d
Kli Kli <klikli@outlook.at>
2026-03-10 13:19:45 +0100

    fix: entry list text align (#327)
    * Fix bookmark text alignment
    
    * fix: improve book list spacing and alignment
    
    - Increase vertical spacing between entries in list screens
    - Reduce the number of entries per page to accommodate larger spacing
    - Vertically center text in buttons when it needs to be scaled down
    - Adjust background highlight dimensions for list items
    
    Fixes #324 - maybe at least

--- common/src/main/java/com/klikli_dev/modonomicon/client/gui/book/search/BookSearchScreen.java
+++ common/src/main/java/com/klikli_dev/modonomicon/client/gui/book/search/BookSearchScreen.java
@@ -36,8 +36,8 @@ import java.util.Comparator;
 import java.util.List;
 
 public class BookSearchScreen extends BookPaginatedScreen {
-    public static final int ENTRIES_PER_PAGE = 13;
-    public static final int ENTRIES_IN_FIRST_PAGE = 11;
+    public static final int ENTRIES_PER_PAGE = 11;
+    public static final int ENTRIES_IN_FIRST_PAGE = 9;
     protected final List<Button> entryButtons = new ArrayList<>();
     protected final BookParentScreen parentScreen;
     private final List<BookEntry> visibleEntries = new ArrayList<>();
@@ -322,7 +322,7 @@ public class BookSearchScreen extends BookPaginatedScreen {
 
     void addEntryButtons(int x, int y, int start, int count) {
         for (int i = 0; i < count && (i + start) < this.visibleEntries.size(); i++) {
-            Button button = new EntryListButton(this.visibleEntries.get(start + i), this.bookLeft + x, this.bookTop + y + i * 11, this::handleButtonEntry);
+            Button button = new EntryListButton(this.visibleEntries.get(start + i), this.bookLeft + x, this.bookTop + y + i * 13, this::handleButtonEntry);
             this.addRenderableWidget(button);
             this.entryButtons.add(button);
         }
