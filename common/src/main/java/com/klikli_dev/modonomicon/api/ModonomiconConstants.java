262e0256b1dc93af361f90df037f9f72b6a9402e
Kli Kli <klikli@outlook.at>
2026-03-22 16:16:28 +0100

    fix: page text renders transparent (#330)

--- common/src/main/java/com/klikli_dev/modonomicon/api/ModonomiconConstants.java
+++ common/src/main/java/com/klikli_dev/modonomicon/api/ModonomiconConstants.java
@@ -42,6 +42,8 @@ public class ModonomiconConstants {
             public static final String DEFAULT_CRAFTING_TEXTURE = Identifier.fromNamespaceAndPath(ModonomiconAPI.ID, "textures/gui/crafting_textures.png").toString();
             public static final String DEFAULT_PAGE_TURN_SOUND = Identifier.fromNamespaceAndPath(ModonomiconAPI.ID, "turn_page").toString();
             public static final String DEFAULT_MODEL = Identifier.fromNamespaceAndPath(ModonomiconAPI.ID, "modonomicon_purple").toString();
+            public static final int DEFAULT_TITLE_COLOR = 0xFF000000;
+            public static final int DEFAULT_TEXT_COLOR = 0xFF000000;
             public static final Identifier ITEM_ID = Identifier.fromNamespaceAndPath(ModonomiconAPI.ID, "modonomicon");
 
         }
