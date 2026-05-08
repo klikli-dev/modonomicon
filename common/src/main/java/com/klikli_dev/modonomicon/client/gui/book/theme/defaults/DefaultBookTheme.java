6a86ff6903c17e737dcd5f69867eedc748dd4d58
Kli Kli <klikli@outlook.at>
2026-05-08 18:31:28 +0200

    fix: use actual frame overlay sprite sizes

--- common/src/main/java/com/klikli_dev/modonomicon/client/gui/book/theme/defaults/DefaultBookTheme.java
+++ common/src/main/java/com/klikli_dev/modonomicon/client/gui/book/theme/defaults/DefaultBookTheme.java
@@ -396,10 +396,10 @@ public class DefaultBookTheme implements BookTheme {
         this.rightArrow = this.sprite("node/connections/right_arrow.png", 30, 30);
         this.leftArrow = this.sprite("node/connections/left_arrow.png", 30, 30);
         this.frameSprite = this.sprite("frame/frame.png", 140, 140);
-        this.topOverlay = new GuiFrameOverlay(this.sprite("frame/top_overlay.png", 72, 7), 0, 4);
-        this.bottomOverlay = new GuiFrameOverlay(this.sprite("frame/bottom_overlay.png", 72, 8), 0, -4);
-        this.leftOverlay = new GuiFrameOverlay(this.sprite("frame/left_overlay.png", 7, 70), 3, 0);
-        this.rightOverlay = new GuiFrameOverlay(this.sprite("frame/right_overlay.png", 8, 70), -4, 0);
+        this.topOverlay = new GuiFrameOverlay(this.sprite("frame/top_overlay.png", -1, -1), 0, 4);
+        this.bottomOverlay = new GuiFrameOverlay(this.sprite("frame/bottom_overlay.png", -1, -1), 0, -4);
+        this.leftOverlay = new GuiFrameOverlay(this.sprite("frame/left_overlay.png", -1, -1), 3, 0);
+        this.rightOverlay = new GuiFrameOverlay(this.sprite("frame/right_overlay.png", -1, -1), -4, 0);
     }
 
     @Override
