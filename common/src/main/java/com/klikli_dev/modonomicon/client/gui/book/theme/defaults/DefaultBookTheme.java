8caf422438b6d6eb25793fd73dfa1cf989ebe57f
Kli Kli <klikli@outlook.at>
2026-05-08 18:47:54 +0200

    fix: align bottom and left frame overlays

--- common/src/main/java/com/klikli_dev/modonomicon/client/gui/book/theme/defaults/DefaultBookTheme.java
+++ common/src/main/java/com/klikli_dev/modonomicon/client/gui/book/theme/defaults/DefaultBookTheme.java
@@ -397,8 +397,8 @@ public class DefaultBookTheme implements BookTheme {
         this.leftArrow = this.sprite("node/connections/left_arrow.png", 30, 30);
         this.frameSprite = this.sprite("frame/frame.png", 140, 140);
         this.topOverlay = new GuiFrameOverlay(this.sprite("frame/top_overlay.png", -1, -1), 0, 8);
-        this.bottomOverlay = new GuiFrameOverlay(this.sprite("frame/bottom_overlay.png", -1, -1), 0, -8);
-        this.leftOverlay = new GuiFrameOverlay(this.sprite("frame/left_overlay.png", -1, -1), 7, 0);
+        this.bottomOverlay = new GuiFrameOverlay(this.sprite("frame/bottom_overlay.png", -1, -1), 0, -10);
+        this.leftOverlay = new GuiFrameOverlay(this.sprite("frame/left_overlay.png", -1, -1), 8, 0);
         this.rightOverlay = new GuiFrameOverlay(this.sprite("frame/right_overlay.png", -1, -1), -8, 0);
     }
 
