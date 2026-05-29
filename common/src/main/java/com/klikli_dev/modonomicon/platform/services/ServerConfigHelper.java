33dad6035dec06c189a7354f4f1110126d774a96
Kli Kli <klikli@outlook.at>
2026-05-29 16:19:00 +0200

    refactor: restore empty server config helpers

---
+++ common/src/main/java/com/klikli_dev/modonomicon/platform/services/ServerConfigHelper.java
@@ -0,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2024 klikli-dev
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.klikli_dev.modonomicon.platform.services;
+
+public interface ServerConfigHelper {
+}
---
+++ fabric/src/main/java/com/klikli_dev/modonomicon/config/FabricServerConfigHelper.java
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: 2024 klikli-dev
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.klikli_dev.modonomicon.config;
+
+import com.klikli_dev.modonomicon.platform.services.ServerConfigHelper;
+
+public class FabricServerConfigHelper implements ServerConfigHelper {
+}
