6df838d8a7c9effaabcf5354703805f8a045ab97
Kli Kli <klikli@outlook.at>
2026-06-08 20:08:14 +0200

    feat: implement getCreatorModId/Namespace

--- neo/src/main/java/com/klikli_dev/modonomicon/platform/NeoPlatformHelper.java
+++ neo/src/main/java/com/klikli_dev/modonomicon/platform/NeoPlatformHelper.java
@@ -6,7 +6,10 @@
 
 package com.klikli_dev.modonomicon.platform;
 
+import com.klikli_dev.modonomicon.item.ModonomiconItem;
+import com.klikli_dev.modonomicon.item.NeoModonomiconItem;
 import com.klikli_dev.modonomicon.platform.services.PlatformHelper;
+import net.minecraft.world.item.Item;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.loading.FMLEnvironment;
@@ -32,5 +35,10 @@ public class NeoPlatformHelper implements PlatformHelper {
     @Override
     public PhysicalSide getPhysicalSide() {
         return FMLEnvironment.getDist() == Dist.CLIENT ? PhysicalSide.CLIENT : PhysicalSide.DEDICATED_SERVER;
+    }
+
+    @Override
+    public ModonomiconItem createModonomiconItem(Item.Properties properties) {
+        return new NeoModonomiconItem(properties);
     }
 }
