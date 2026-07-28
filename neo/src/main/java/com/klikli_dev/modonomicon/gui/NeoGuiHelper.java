92b3ccee86205e6a41aa48f98e09000aaee19399
Kli Kli <klikli@outlook.at>
2026-07-28 14:04:37 +0200

    fix: use NeoForge GUI layer hooks

--- neo/src/main/java/com/klikli_dev/modonomicon/gui/NeoGuiHelper.java
+++ neo/src/main/java/com/klikli_dev/modonomicon/gui/NeoGuiHelper.java
@@ -9,16 +9,17 @@ package com.klikli_dev.modonomicon.gui;
 import com.klikli_dev.modonomicon.platform.services.GuiHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import net.neoforged.neoforge.client.ClientHooks;
 
 public class NeoGuiHelper implements GuiHelper {
     @Override
     public void pushGuiLayer(Screen screen) {
-        Minecraft.getInstance().pushGuiLayer(screen);
+        ClientHooks.pushGuiLayer(Minecraft.getInstance(), screen);
     }
 
     @Override
     public void popGuiLayer() {
-        Minecraft.getInstance().popGuiLayer();
+        ClientHooks.popGuiLayer(Minecraft.getInstance());
     }
 
     @Override
