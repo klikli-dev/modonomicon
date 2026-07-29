99bdd5ac0f049d19b9686f3dbefbe70efa54ed43
Kli Kli <klikli@outlook.at>
2026-07-29 16:22:23 +0200

    feat: switch (back) to neo's multi layer gui

--- neo/src/main/java/com/klikli_dev/modonomicon/gui/NeoGuiHelper.java
+++ neo/src/main/java/com/klikli_dev/modonomicon/gui/NeoGuiHelper.java
@@ -11,58 +11,19 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 
 public class NeoGuiHelper implements GuiHelper {
-    private static MultiLayerScreen multiLayerScreen;
 
     @Override
     public void pushGuiLayer(Screen screen) {
-        var minecraft = Minecraft.getInstance();
-        var oldScreen = minecraft.gui.screen();
-
-        if (multiLayerScreen == null) {
-            multiLayerScreen = new MultiLayerScreen();
-        }
-
-        if (oldScreen != multiLayerScreen) {
-            multiLayerScreen.guiLayers.clear();
-            multiLayerScreen.guiLayers.push(oldScreen);
-        }
-
-        multiLayerScreen.guiLayers.push(screen);
-
-        if (oldScreen != multiLayerScreen) {
-            multiLayerScreen.init(minecraft.getWindow().getGuiScaledWidth(), minecraft.getWindow().getGuiScaledHeight());
-            minecraft.gui.setScreen(multiLayerScreen);
-        }
-
-        screen.init(minecraft.getWindow().getGuiScaledWidth(), minecraft.getWindow().getGuiScaledHeight());
-        minecraft.getNarrator().saySystemNow(screen.getNarrationMessage());
+        Minecraft.getInstance().gui.pushScreenLayer(screen);
     }
 
     @Override
     public void popGuiLayer() {
-        var minecraft = Minecraft.getInstance();
-        if (multiLayerScreen == null || minecraft.gui.screen() != multiLayerScreen) {
-            return;
-        }
-
-        if (multiLayerScreen.guiLayers.size() == 1) {
-            minecraft.gui.setScreen(null);
-        }
-
-        var removed = multiLayerScreen.guiLayers.pop();
-        removed.removed();
-
-        if (!multiLayerScreen.guiLayers.isEmpty()) {
-            minecraft.getNarrator().saySystemNow(multiLayerScreen.guiLayers.peek().getNarrationMessage());
-        }
+        Minecraft.getInstance().gui.popScreenLayer();
     }
 
     @Override
     public Screen getCurrentScreen() {
-        if (multiLayerScreen == null) {
-            return Minecraft.getInstance().gui.screen();
-        }
-
-        return multiLayerScreen.guiLayers.isEmpty() ? Minecraft.getInstance().gui.screen() : multiLayerScreen.guiLayers.peek();
+        return Minecraft.getInstance().gui.screen();
     }
 }
